01b03f7554dbca2bb512dfcf08d081d2ba0f6e89
dvonthenen <david.vonthenen@emc.com>
2016-09-19 12:50:32 -0700

    Found bug with script... did not update version in json files.

--- version.sh
+++ version.sh
@@ -11,6 +11,7 @@ fi
 if [ "$#" -eq 1 ]; then
   grep -R --exclude-dir vendor --exclude-dir .git --exclude-dir mesos --exclude scaleio-executor --exclude scaleio-scheduler --exclude build.sh $1 ./
 else
+  find . -type f -name '*.json' -exec sed -i '' 's/'"$1"'/'"$2"'/g' {} +
   find . -type f -name '*.md' -exec sed -i '' 's/'"$1"'/'"$2"'/g' {} +
   find . -type f -name '*.go' -exec sed -i '' 's/'"$1"'/'"$2"'/g' {} +
   find . -type f -name 'VERSION' -exec sed -i '' 's/'"$1"'/'"$2"'/g' {} +
